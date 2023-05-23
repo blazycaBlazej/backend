32dcfc5e8301932f4cb5733fc77a2a9f0c833258
Błażej Błażyca <blazycablazej@gmail.com>
2023-05-23 11:48:49 +0200

    sfsgf

--- api.js
+++ api.js
@@ -38,6 +38,19 @@ const connection = mysql.createConnection({
 const app = express()
 app.use(cors())
 
+app.use(function (req, res, next) {
+	// res.header("Access-Control-Allow-Origin", "*");
+	const allowedOrigins = ['http://localhost:3000', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com']
+	const origin = req.headers.origin
+	if (allowedOrigins.includes(origin)) {
+		res.setHeader('Access-Control-Allow-Origin', origin)
+	}
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+	res.header('Access-Control-Allow-credentials', true)
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE')
+	next()
+})
+
 app.listen(port, () => {
 	console.log('Server slucha')
 })
