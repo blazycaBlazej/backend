7c019367066a1087a8fe3918f02aa169b1ace778
Błażej Błażyca <blazycablazej@gmail.com>
2023-05-23 10:59:40 +0200

    AFasf

--- api.js
+++ api.js
@@ -68,6 +68,12 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	next()
+})
+
 app.post('/register', (req, res) => {
 	const { email, password, fullName } = req.body
 
